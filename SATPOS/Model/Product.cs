45b2b9d4ea5815a9c555d15c35322f8cdf92ae3c
Shonal Fernando <snirash.fernando@outlook.com>
2022-06-17 17:45:26 +0530

    minor changes

---
+++ SATPOS/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SATPOS.Model
+{
+    public class Product
+    {
+        public int    Prd_ID        { get; set; }    
+        public string Prd_Name      { get; set; }    
+        public int    Prd_Price     { get; set; }    
+        public double Prd_Type      { get; set; }
+        public string Quantity      { get; set; }
+    }
+}
---
+++ SATPOS/ViewModel/InvoiceDataLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SATPOS.Model;
+
+namespace SATPOS.ViewModel
+{
+    class InvoiceDataLoader
+    {
+        private List<Product> LoadProductEntry()
+        {
+            List<Product> product = new List<Product>();
+            product.Add(new Product()
+            {
+                Prd_ID = 000001,
+            });
+
+
+
+
+
+            return product;
+        }
+    }
+}
