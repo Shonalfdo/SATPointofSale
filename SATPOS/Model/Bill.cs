40a84dacbff22785b3f84470240eb283dc351c6e
Shonal Fernando <snirash.fernando@outlook.com>
2022-06-17 17:13:54 +0530

    View | Model added 2 classes

---
+++ SATPOS/Model/Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SATPOS.Model
+{
+    public class Bill
+    {
+        public int    BillNo        { get; set; }    
+        public string BillDate      { get; set; }    
+        public int    BillDiscount  { get; set; }    
+        public double Quantity      { get; set; }
+        public string BillType      { get; set; }
+    }
+}
---
+++ SATPOS/Model/Users.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SATPOS.Model
+{
+    class users
+    {
+    }
+}
